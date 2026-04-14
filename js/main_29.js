db921fcbe09cfac97559ad89a8c1a0d579e8bdfe
Iryna-Mykhailovska <i.mihaylovskaya05031983@gmail.com>
2026-04-14 17:50:59 +0300

    add array normal

--- js/main_29.js
+++ js/main_29.js
@@ -83,3 +83,30 @@ function purchaseProduct(list, productName) {
 purchaseProduct(shoppingList, 'eggs');
 displayShoppingList(shoppingList);
 purchaseProduct(shoppingList, 'meat');
+/*Видалення продукту зі списку (видалення повинно проводитися шляхом створення нового масиву, 
+в якому продукт, що ми шукаємо, буде відсутнім)
+Додавання покупки в список. Враховуй, що при додаванні покупки з уже існуючим в списку продуктом, 
+необхідно збільшувати кількість в існуючій покупці, а не додавати нову. При цьому також повинна змінитися сума, 
+наприклад, якщо ціна за одиницю 12, а кількості товарів стало 2, то сума буде 24.
+ */
+function removeProduct(list, productName) {
+    const newList = list.filter(item => item.name !== productName);
+    console.log(`Продукт "${productName}" видалено зі списку.`);
+    return newList;
+}   
+shoppingList = removeProduct(shoppingList, 'milk');
+displayShoppingList(shoppingList);  
+function addProduct(list, product) {
+    const existingProduct = list.find(item => item.name === product.name);  
+    if (existingProduct) {
+        existingProduct.quantity += product.quantity;
+        console.log(`Кількість продукту "${product.name}" оновлено. Нова кількість: ${existingProduct.quantity}.`);
+    } else {
+        list.push(product);
+        console.log(`Продукт "${product.name}" додано до списку.`);
+    }       
+}
+addProduct(shoppingList, { name: 'bread', quantity: 1, purchased: false, price: 25.5 });
+addProduct(shoppingList, { name: 'meat', quantity: 2, purchased: false, price: 150 });
+addProduct(shoppingList, { name: 'cake', quantity: 1, purchased: false, price: 320 });
+displayShoppingList(shoppingList);  
