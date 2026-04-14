9348a5d68cf0364ca173fd677c13d3af5d693f44
Iryna-Mykhailovska <i.mihaylovskaya05031983@gmail.com>
2026-04-14 23:56:14 +0300

    add hwIvan

--- js/main_30.js
+++ js/main_30.js
@@ -43,3 +43,53 @@ playList.forEach(item => {
     ol.append(li);
 });
 blockOutput.append(ol);  
+/* */
+let shoppingList = [
+    {
+        name: 'bread',
+        quantity: 2,
+        purchased: false,
+        price: 25.5,
+    },
+    {
+        name: 'milk',
+        quantity: 2,
+        purchased: true,
+        price: 43.90,
+        // get total() {
+        //     return this.quantity * this.price;
+        // },
+    },
+    {
+        name: 'eggs',
+        quantity: 10,
+        purchased: false,
+        price: 8.5,
+        // get total() {
+        //     return this.quantity * this.price;
+        // }
+    },
+    {
+        name: 'chicken',
+        quantity: 1,
+        purchased: true,
+        price: 201,
+        // get total() {
+        //     return this.quantity * this.price;
+        // }
+    },
+];
+const title = document.createElement('h2');
+title.textContent = 'Список покупок';
+title.classList.add('title');
+blockOutput.append(title);
+const listFoods = document.createElement('ul');
+listFoods.classList.add('listFoods');
+shoppingList.forEach(item => {
+    const li = document.createElement('li');        
+    li.textContent = `${item.name} - ${item.quantity} шт. * ${item.price} грн. = ${item.quantity * item.price} грн. - ${item.purchased ? 'куплено' : 'не куплено'}`;
+    listFoods.append(li);
+});
+blockOutput.append(listFoods);
+
+
