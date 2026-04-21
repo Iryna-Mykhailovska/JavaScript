4be272e14b3f89e5671f7293139e3f8720fb6f4c
Iryna-Mykhailovska <i.mihaylovskaya05031983@gmail.com>
2026-04-21 19:30:01 +0300

    fix

--- js/main_31.js
+++ js/main_31.js
@@ -1,12 +1,23 @@
 'use strict';
 /* Додати поле вводу,якщо змінюється вводиме значення, 
-то виводити одразу те, що вводиться, а без повтору попереднього
+то виводити одразу в параграф те, що вводиться, а без повтору попереднього
  та кнопку , чкщо буде очищати поле вводу, та поле виводу
    */
 
 const inputText = document.getElementById('text-input');
+const outputParagraph = document.getElementById('output');  
 function addSymbol(name) {
     let textInInput = name.value;
     console.log(textInInput);
-    
+    outputParagraph.textContent = textInInput;
 }
+inputText.addEventListener('input', function() {
+    addSymbol(inputText);
+});
+
+const clearBtn = document.getElementById('clear-btn');      
+clearBtn.addEventListener('click', function() {
+    inputText.value = '';
+    outputParagraph.textContent = '';
+}); 
+
